1144b6e78d46863551f0b57355649647cdc0bfc4
Thiago <tfbellato@hotmail.com>
2025-08-26 08:55:18 -0300

    att

--- logica-de-programacao/aula07/modulos_bibliotecas.py
+++ logica-de-programacao/aula07/modulos_bibliotecas.py
@@ -11,14 +11,14 @@ import os
 # pyautogui.press("win")
 # pyautogui.write("vscode")
 
-lista_arquivos = os.listdir("../arquivos")
+lista_arquivos = os.listdir("arquivos")
 print(lista_arquivos)
 
 for arquivo in lista_arquivos:
     if "txt" in arquivo:
         if "22" in arquivo:
-            os.rename(f"../arquivos/{arquivo}", f"../arquivos/22/{arquivo}")
+            os.rename(f"arquivos/{arquivo}", f"arquivos/22/{arquivo}")
             print("Movimentar para a pasta 22", arquivo)
         elif "23" in arquivo:
-            os.rename(f"../arquivos/{arquivo}", f"../arquivos/23/{arquivo}")
+            os.rename(f"arquivos/{arquivo}", f"arquivos/23/{arquivo}")
             print("Movimentar para a pasta 23", arquivo)
