f87c408159d2d702a80a312af69603973779a39f
Thiago <tfbellato@hotmail.com>
2025-08-26 11:29:11 -0300

    att

--- exercicios/01.py
+++ exercicios/01.py
@@ -1,18 +1,8 @@
-
-# ▪ Calcule o valor total da venda (quantidade * preço).
-# ▪ Imprima uma mensagem de sucesso, como "Venda de X
-# unidades de Y realizada com sucesso. Total: R$ Z".
-# ▪ Se não houver quantidade suficiente:
-# ▪ Imprima uma mensagem informando que não há estoque
-# suficiente.
-# ▪ Se o produto não existir:
-# ▪ Imprima uma mensagem de erro, como "Produto não
-# encontrado.".
 # 4. Crie uma função para calcular impostos:
 # o Use o conceito de def para criar uma função chamada
 # calcular_imposto(preco).
-# o Esta função deve receber o preço de um produto como parâmetro.
-# o A função deve retornar o valor do imposto, que é de 15% sobre o preço.
+# o Esta função deve receber o preco de um produto como parâmetro.
+# o A função deve retornar o valor do imposto, que é de 15% sobre o preco.
 # o No final da sua lógica de venda, adicione uma linha para calcular e exibir
 # o valor do imposto sobre a venda total.
 # 5. Exiba o estoque final e o lucro total:
@@ -21,15 +11,23 @@
 # o Calcule o lucro total com a venda realizada (Venda Total - Imposto Total).
 # o Imprima o valor do lucro total.
 
+
+def calcular_imposto(preco):
+
+    imposto = 0.15
+    valor_total_venda = preco * imposto
+    print(valor_total_venda)
+
+
 estoque_loja = {
-    "monitor": {"quantidade": 15, "preço": 850.50},
-    "teclado": {"quantidade": 25, "preço": 120.00},
-    "mouse": {"quantidade": 30, "preço": 85.00},
+    "monitor": {"quantidade": 15, "preco": 850.50},
+    "teclado": {"quantidade": 25, "preco": 120.00},
+    "mouse": {"quantidade": 30, "preco": 85.00},
 }
 
 for nome_produto, detalhes_produto in estoque_loja.items():
     print(
-        f"- {nome_produto.capitalize()}: {detalhes_produto['quantidade']} - {detalhes_produto['preço']}"
+        f"- {nome_produto.capitalize()}: {detalhes_produto['quantidade']} - {detalhes_produto['preco']}"
     )
 
 nome_produto = input("Qual produto deseja comprar:").lower()
@@ -37,16 +35,18 @@ quantidade_digitada = int(input("Qual a quantidade que você deseja comprar:"))
 
 if nome_produto not in estoque_loja:
     print("Produto Não Encontrado")
-elif estoque_loja[nome_produto]['quantidade'] >= quantidade_digitada:
-    estoque_loja[nome_produto]['quantidade'] -= quantidade_digitada
-    for nome_produto, detalhes_produto in estoque_loja.items():
-        print(
-            f"- {nome_produto.capitalize()}: {detalhes_produto['quantidade']} - {detalhes_produto['preço']}"
-        )
-    print(f"Venda realizada de R$ {quantidade_digitada*detalhes_produto['preço']}")
+elif estoque_loja[nome_produto]["quantidade"] >= quantidade_digitada:
+    estoque_loja[nome_produto]["quantidade"] -= quantidade_digitada
+    preco_unitario = estoque_loja[nome_produto]["preco"]
+    valor_total_venda = quantidade_digitada * preco_unitario
+    print(
+        f"Venda de {quantidade_digitada} unidades de {nome_produto.capitalize()} realizada com sucesso."
+    )
+    print(f"Total: R$ {valor_total_venda:.2f}")
+
 else:
     print("Não temos a quantidade desejada em estoque.")
-    
-    
-# def calcular_imposto()
 
+for preco in estoque_loja:
+    imposto = calcular_imposto(preco)
+    print(calcular_imposto)
--- logica-de-programacao/aula08/gerenciador_backup.py
+++ logica-de-programacao/aula08/gerenciador_backup.py
@@ -6,5 +6,19 @@ import shutil
 nome_pasta_selecionada = askdirectory()
 print(nome_pasta_selecionada)
 
-lsita_arquivos = os.listdir(nome_pasta_selecionada)
-print(lsita_arquivos)
+lista_arquivos = os.listdir(nome_pasta_selecionada)
+print(lista_arquivos)
+
+nome_pasta_backup = "backup"
+nome_completo_pasta_backup = f"{nome_pasta_selecionada}/{nome_pasta_backup}"
+if not os.path.exists(nome_completo_pasta_backup):
+    os.mkdir(nome_completo_pasta_backup)
+
+for arquivo in lista_arquivos:
+    print(arquivo)
+    nome_completo_arquivo = f"{nome_pasta_selecionada}/{arquivo}"
+    nome_final_arquivo = f"{nome_completo_pasta_backup}/{arquivo}"
+    if "." in arquivo:
+        shutil.copy2(nome_completo_arquivo, nome_final_arquivo)
+    elif "backup" != arquivo:
+        shutil.copytree(nome_completo_arquivo, nome_final_arquivo)
