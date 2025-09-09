2d733fa04fdc3883bda7fa4ed50ba22e4c8e7df9
Stephanie Souza <tfbellato@hotmail.com>
2025-09-09 11:31:10 -0300

    att

---
+++ logica-de-programacao/aula10/automacao_03.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+navegador = webdriver.Chrome()
+navegador.get("https://ge.globo.com")
+navegador.maximize_window()
+
+botao_menu = navegador.find_element("class name", "menu-button")
+botao_menu.click()
+
+time.sleep(10)
