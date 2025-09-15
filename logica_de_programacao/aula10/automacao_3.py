1a16b58bbb1fc52667e3eae3f4cfc1a26c621d9a
Stephanie Souza <souzastephaniesec436@gmail.com>
2025-09-15 11:29:26 -0300

    att

--- logica_de_programacao/aula10/automacao_3.py
+++ logica_de_programacao/aula10/automacao_3.py
@@ -7,4 +7,4 @@ navegador.maximize_window()
 botao_menu = navegador.find_element("class name", "menu-button")
 botao_menu.click()
 
-time.sleep(10)
+# time.sleep(10)
---
+++ poo/aula01/classes.py
@@ -0,0 +1,8 @@
+vendedor = "João"
+vendas = 1000
+meta = 500
+
+if vendas >= meta:
+    print("Bateu a Meta")
+else:
+    print("Não Bateu a Meta")
