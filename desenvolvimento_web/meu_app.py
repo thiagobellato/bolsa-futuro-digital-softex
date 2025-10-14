d0a7134f1570377eff534cdc21bc1dc82b2dad9a
Thiago <tfbellato@hotmail.com>
2025-10-14 11:59:21 -0300

    chore: aula 14-10

--- desenvolvimento_web/meu_app.py
+++ desenvolvimento_web/meu_app.py
@@ -10,10 +10,20 @@ with st.container():
     st.write(
         "Site da empresa na B3 [Clique aqui](https://br.tradingview.com/symbols/BMFBOVESPA-GOLL54/?utm_campaign=hotlists&utm_medium=widget&utm_source=www.b3.com.br)"
     )
-    
+
+
+@st.cache_data
+def carragar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
+
+
 with st.container():
+    qtde_dias = st.selectbox(
+        "Selecione o período", ["7 dias", "15 dias", "21 dias", "30 dias"]
+    )
+    num_dias = int(qtde_dias.replace("dias",""))
+    dados = carragar_dados()
+    dados = dados[-num_dias:]
     st.write("---")
-    
-with st.container():
-    dados= pd.read_csv("resultados.csv")
-    st.area_chart(dados ,x="Data" , y="Contratos")
+    st.area_chart(dados, x="Data", y="Contratos")
